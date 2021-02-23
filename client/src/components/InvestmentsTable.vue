03b8717178431dffb7032f2c14b30efe4053b5e6
vystrcild <vystrcil.dusan@gmail.com>
2021-02-23 12:24:03 +0100

    Investments table completed

--- client/src/components/InvestmentsTable.vue
+++ client/src/components/InvestmentsTable.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- INVESTMENTS TABLE -->
-    <div class="bg-virtus-nav rounded-md text-gray-300 shadow-xl mt-8">
+    <div class="bg-virtus-nav rounded-md text-gray-300 shadow-xl mr-4">
 
         <table class="table-fixed w-full" id="investments">
             <thead class="pl-4 bg-virtus-header">
@@ -22,33 +22,29 @@
                   <td class="text-center">{{ item.date }}</td>
                   <td class="text-right">{{ item.investment | formatCZK }}</td>
                   <td class="text-right">{{ item.volume }}</td>
-                  <td v-if="item.ticker == 'BTC'" class="text-right">{{ item.volume * getRealValueBTC | formatCZK }}</td>
-                  <td v-else-if="item.ticker == 'LTC'" class="text-right">{{ item.volume * getRealValueLTC | formatCZK }}</td>
-                  <td v-else-if="item.ticker == 'ETH'" class="text-right">{{ item.volume * getRealValueETH | formatCZK }}</td>
-                  <td v-else class="text-right">NOPE</td>
-                  <td class="text-right">PROFIT</td>
-                  <td class="text-right pr-8">ROI</td>
+                  <td v-if="item.ticker == 'BTC'" class="text-right">{{ (item.value = item.volume * getRealValueBTC * (1-item.costs)) | formatCZK }}</td>
+                  <td v-else-if="item.ticker == 'LTC'" class="text-right">{{ (item.value = item.volume * getRealValueLTC * (1-item.costs)) | formatCZK }}</td>
+                  <td v-else-if="item.ticker == 'ETH'" class="text-right">{{ (item.value = item.volume * getRealValueETH * (1-item.costs)) | formatCZK }}</td>
+                  <td v-else class="text-right">Missing value</td>
+                  <td class="text-right">{{ item.value - item.investment | formatCZK }}</td>
+                  <td class="text-right pr-8">{{ (item.value/item.investment).toFixed(2) }}</td>
                 </tr>
-
 
                 <tr class="bg-virtus-header font-medium opacity-80">
                   <td class="pl-4 pt-2 pb-2 rounded-bl font-extralight text-xs">TOTAL</td>
                   <td class="text-right"></td>
-                  <td class="text-right style-CZK">INVEST TOTAL</td>
+                  <td class="text-right">{{ investmentTotal | formatCZK }}</td>
                   <td class="text-right"></td>
-                  <td class="text-right">VALUE TOTAL</td>
-                  <td class="text-right">PROFIT TOTAL</td>
-                  <td class="text-right rounded-br pr-8">Ahoj()</td>
+                  <td class="text-right">{{ totalValue | formatCZK }}</td>
+                  <td class="text-right">{{ totalValue - investmentTotal | formatCZK }}</td>
+                  <td class="text-right rounded-br pr-8">{{ (totalValue/investmentTotal).toFixed(2) }}</td>
                 </tr>
+
           </tbody>
         </table>
         <!-- END OF INVESTMENTS TABLE -->
-  </div>
-    <p>{{ USDCZKrate }}</p>
-    <p>{{ BTCUSDrate }}</p>
-    <p>{{ getRealValueBTC }}</p>
     </div>
-
+  </div>
 </template>
 
 <script>
@@ -63,12 +59,14 @@ export default {
         investment: 0,
         date: 0,
         volume: 0,
-        id: 0
+        id: 0,
+        value: 0
       },
       USDCZKrate: 0,
       BTCUSDrate: 0,
       LTCUSDrate: 0,
       ETHUSDrate: 0,
+      totalValue: 0,
     }
   },
   methods: {
@@ -79,30 +77,52 @@ export default {
           this.investments = res.data.investments;
         })
     },
+    valueTotal() {
+      let sum = 0
+      for (let el in this.investments) {
+        sum += parseFloat( this.investments[el].value);
+      }
+      this.totalValue = sum
+    },
+    getCryptoRates() {
+      axios.get('https://api.coincap.io/v2/rates/bitcoin')
+        .then(response => (this.BTCUSDrate = response.data.data.rateUsd));
+      axios.get('https://api.coincap.io/v2/rates/litecoin')
+        .then(response => (this.LTCUSDrate = response.data.data.rateUsd));
+      axios.get('https://api.coincap.io/v2/rates/ethereum')
+        .then(response => (this.ETHUSDrate = response.data.data.rateUsd));
+    }
   },
   created() {
     this.getInvestments();
+    this.interval = setInterval(() => this.getCryptoRates(),5000);
+  },
+  updated() {
+    this.valueTotal();
   },
   mounted() {
     axios.get('https://api.exchangeratesapi.io/latest?base=USD&symbols=CZK')
       .then(response => (this.USDCZKrate = response.data.rates.CZK));
-    axios.get('https://api.coincap.io/v2/rates/bitcoin')
-      .then(response => (this.BTCUSDrate = response.data.data.rateUsd));
-    axios.get('https://api.coincap.io/v2/rates/litecoin')
-      .then(response => (this.LTCUSDrate = response.data.data.rateUsd));
-    axios.get('https://api.coincap.io/v2/rates/ethereum')
-      .then(response => (this.ETHUSDrate = response.data.data.rateUsd));
+    this.getCryptoRates()
   },
   computed: {
     getRealValueBTC: function(){
-      return this.rate = this.BTCUSDrate * this.USDCZKrate
+      return this.BTCUSDrate * this.USDCZKrate
     },
     getRealValueLTC: function(){
-      return this.rate = this.LTCUSDrate * this.USDCZKrate
+      return this.LTCUSDrate * this.USDCZKrate
     },
     getRealValueETH: function(){
-      return this.rate = this.ETHUSDrate * this.USDCZKrate
+      return this.ETHUSDrate * this.USDCZKrate
     },
+    investmentTotal: function() {
+      let sum = 0
+      for (let el in this.investments) {
+        sum += parseFloat( this.investments[el].investment);
+      }
+      return sum
+    },
+
 
   }
 }
